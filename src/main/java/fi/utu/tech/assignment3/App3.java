2cfbf00e8ceb70df23fa44405b038d05d7fb455f
SipuliCodes <vepsalainensisu@gmail.com>
2024-11-02 12:22:54 +0200

    tehtävä 3 valmis

--- src/main/java/fi/utu/tech/assignment3/App3.java
+++ src/main/java/fi/utu/tech/assignment3/App3.java
@@ -12,5 +12,45 @@ public class App3 {
     public static void main( String[] args )
     {
         // Kopioi edellisen tehtävän ratkaisu tähän lähtökohdaksi
+        // Otetaan funktion aloitusaika talteen suoritusajan laskemista varten
+        long startTime = System.currentTimeMillis();
+
+        // Generoidaan kasa esimerkkitehtäväpalautuksia
+        List<Submission> ungradedSubmissions = SubmissionGenerator.generateSubmissions(21, 200, Strategy.STATIC);
+
+        // Tulostetaan tiedot esimerkkipalautuksista ennen arviointia
+        for (var ug : ungradedSubmissions) {
+            System.out.println(ug);
+        }
+
+        // Luodaan uusi arviointitehtävä
+        GradingTask gradingTask = new GradingTask();
+        Thread gradingTaskThread = new Thread(gradingTask);
+        // Annetaan palautukset gradeAll-metodille ja saadaan arvioidut palautukset takaisin
+        // List<Submission> gradedSubmissions =  gradingTask.gradeAll(ungradedSubmissions);
+        /*
+         * TODO: Muokkaa common-pakkauksen GradingTask-luokkaa siten,
+         * että alla oleva run()-metodi (ilman argumentteja!) tarkistaa palautukset (ungradedSubmissions).
+         * Yllä olevaa gt.gradeAll()-metodia ei tule enää käyttää suoraan
+         * tästä main-metodista. Tarkemmat ohjeet tehtävänannossa.
+         * Joudut keksimään, miten GradingTaskille voi antaa tehtävät ja miten ne siltä saa noukittua
+         */
+        gradingTask.setUngradedSubmissions(ungradedSubmissions);
+        gradingTaskThread.start();
+        try {
+            gradingTaskThread.join();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        List<Submission> gradedSubmissions = gradingTask.getGradedSubmissions();
+
+        // Tulostetaan arvioidut palautukset
+        System.out.println("------------ CUT HERE ------------");
+        for (var gs : gradedSubmissions) {
+            System.out.println(gs);
+        }
+
+        // Lasketaan funktion suoritusaika
+        System.out.printf("Total time for grading: %d ms%n", System.currentTimeMillis()-startTime);
     }
 }
