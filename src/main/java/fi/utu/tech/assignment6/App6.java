53c16975604d945d89c108d4b38b90d4e8a068a4
SipuliCodes <vepsalainensisu@gmail.com>
2024-11-04 12:07:11 +0200

    tehtävä 6 valmis

--- src/main/java/fi/utu/tech/assignment6/App6.java
+++ src/main/java/fi/utu/tech/assignment6/App6.java
@@ -24,5 +24,60 @@ public class App6 {
         // Voit käyttää yllä olevaa riviä palautusten generointiin. Se ei vaikuta ratkaisuun, mutta
         // "epäreilu" strategia tekee yhdestä palautuksesta paljon muita haastavamman tarkistettavan,
         // demonstroiden dynaamisen työnjaon etuja paremmin.
+
+        // Kopioi edellisen tehtävän ratkaisu tähän lähtökohdaksi
+        // Tässä tehtävässä vaaditaan myös muutoksia TaskAllocator-luokkaan
+        // Otetaan funktion aloitusaika talteen suoritusajan laskemista varten
+        long startTime = System.currentTimeMillis();
+
+        // Generoidaan kasa esimerkkitehtäväpalautuksia
+        // List<Submission> ungradedSubmissions = SubmissionGenerator.generateSubmissions(21, 200, Strategy.STATIC);
+
+        // Tulostetaan tiedot esimerkkipalautuksista ennen arviointia
+        for (var ug : ungradedSubmissions) {
+            System.out.println(ug);
+        }
+
+        // Luodaan uusi arviointitehtävä
+        // Annetaan palautukset gradeAll-metodille ja saadaan arvioidut palautukset takaisin
+        // List<Submission> gradedSubmissions =  gradingTask.gradeAll(ungradedSubmissions);
+        /*
+         * TODO: Muokkaa common-pakkauksen GradingTask-luokkaa siten,
+         * että alla oleva run()-metodi (ilman argumentteja!) tarkistaa palautukset (ungradedSubmissions).
+         * Yllä olevaa gt.gradeAll()-metodia ei tule enää käyttää suoraan
+         * tästä main-metodista. Tarkemmat ohjeet tehtävänannossa.
+         * Joudut keksimään, miten GradingTaskille voi antaa tehtävät ja miten ne siltä saa noukittua
+         */
+        List<GradingTask> gradingTasks = TaskAllocator.allocate(ungradedSubmissions, 21);
+
+        ExecutorService pool = Executors.newFixedThreadPool(8);
+        for( var gt : gradingTasks) {
+            pool.execute(gt);
+        }
+
+        pool.shutdown();
+
+        try{
+            if(!pool.awaitTermination(10, TimeUnit.SECONDS)) {
+                pool.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            pool.shutdownNow();
+            System.out.println(e.getMessage());
+        }
+
+        List<Submission> gradedSubmissions = new ArrayList<>();
+        for(var gt : gradingTasks) {
+            gradedSubmissions.addAll(gt.getGradedSubmissions());
+        }
+
+        // Tulostetaan arvioidut palautukset
+        System.out.println("------------ CUT HERE ------------");
+        for (var gs : gradedSubmissions) {
+            System.out.println(gs);
+        }
+
+        // Lasketaan funktion suoritusaika
+        System.out.printf("Total time for grading: %d ms%n", System.currentTimeMillis()-startTime);
     }
 }
